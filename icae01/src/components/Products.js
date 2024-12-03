a7440853ce7417618dbc57a5064bdebade07763f
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2024-12-03 16:29:01 +0000

    Products file updated

--- icae01/src/components/Products.js
+++ icae01/src/components/Products.js
@@ -1,29 +1,40 @@
+import React, { useState } from "react";
+import "../assets/CSS/layout.css";
+import Product from "./Product";
+import Cart from "./Cart";
+import { flowers } from "./FlowerDB";
 
-import '../assets/CSS/layout.css';
+export default function Products() {
+  const [cartItems, setCartItems] = useState([]);
 
+  const addToCart = (flower, quantity) => {
+    const updatedCart = [...cartItems];
+    const itemIndex = updatedCart.findIndex(item => item.id === flower.id);
 
-export default function Products(){
-   
-    return(
-        <>
-            <div className="item1">
-                <h1>Flower Shop</h1>
-            </div>
-            <div className="item2">
-                <h4 className="card-title">Buy flowers</h4>
-                <div className="grid-container">
-                    {
-                        //product
-                    }
-                </div>
+    if (itemIndex > -1) {
+      updatedCart[itemIndex].qty += quantity;
+    } else {
+      updatedCart.push({ ...flower, qty: quantity });
+    }
+    setCartItems(updatedCart);
+  };
 
-            </div>
-            <div className="item3">
-                {
-                //cart
-                }
-            </div>
-        </>
-    );
-
+  return (
+    <>
+      <div className="item1">
+        <h1>Flower Shop</h1>
+      </div>
+      <div className="item2">
+        <h4 className="card-title">Buy flowers</h4>
+        <div className="grid-container">
+          {flowers.map(flower => (
+            <Product key={flower.id} flower={flower} addToCart={addToCart} />
+          ))}
+        </div>
+      </div>
+      <div className="item3">
+        <Cart cartItems={cartItems} />
+      </div>
+    </>
+  );
 }
