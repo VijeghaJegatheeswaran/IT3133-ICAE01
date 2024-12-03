de8bed9d362333bff19c9326c066a79753e8e93b
VijeghaJegatheeswaran <139634071+VijeghaJegatheeswaran@users.noreply.github.com>
2024-12-03 16:28:07 +0000

    Cart created

--- icae01/src/components/Cart.js
+++ icae01/src/components/Cart.js
@@ -1,25 +1,34 @@
-export default function Cart() {
-    
-    return (
-        <div className="table-container">
-            <h4 className="card-title">Cart</h4>
-            <table>
-                <thead>
+import React from "react";
 
-                    <td>Product</td>
-                    <td>QTY</td>
-                    <td>Price</td>
+export default function Cart({ cartItems }) {
+  const calculateTotal = () =>
+    cartItems.reduce((total, item) => total + item.price * item.qty, 0);
 
-                </thead>
-                {
-                    
-                }
-                <tr>
-                    <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
-                </tr>
-            </table>
-           
-        </div>
-    );
+  return (
+    <div className="table-container">
+      <h4 className="card-title">Cart</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>QTY</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map(item => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.qty}</td>
+              <td>${item.price * item.qty}</td>
+            </tr>
+          ))}
+          <tr>
+            <td>Grand Total:</td>
+            <td colSpan={2}>${calculateTotal()}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
